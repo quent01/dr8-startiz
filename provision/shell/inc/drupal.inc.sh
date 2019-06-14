4eedb125519fbfc846cd2c0b4c556bfce0b7a981
qthenoz <quentinthenoz@gmail.com>
2019-06-14 19:31:30 +0200

    first commit : we build around the scotchbox

---
+++ provision/shell/inc/drupal.inc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# drupal.inc.sh
+#
+
+function drupal_already_installed(){
+    if [[ -e $FILE_COMPOSER ]]; then
+        echo "true"
+    fi
+    echo "false"
+}
+
+# Installation of composer dependencies
+# in the case we pull the project but we never build it
+function drupal_install_dependencies(){
+    alert_info "Installation of Drupal 8 dependencies ..."
+    alert_info "$(alert_line)"    
+    composer --global config process-timeout 2000
+    composer install --prefer-dist
+    alert_success "Drupal 8 dependencies were installed with success..."
+    alert_success "$(alert_line)"
+}
+
+function drupal_install(){
+    alert_info "Installation of Drupal 8..."
+    alert_info "$(alert_line)"    
+    
+    composer create-project drupal-composer/drupal-project:8.x-dev ./ --stability dev --no-interaction
+    alert_success "Drupal 8 was downloaded with success."    
+    drush site-install standard --db-url=mysql://${DB_USER}:${DB_PASS}@localhost/${DB_NAME} --account-name=${ADMIN_USER} --account-pass=${ADMIN_PWD}
+
+    alert_success "Drupal 8 has been installed with success."
+    alert_success "$(alert_line)"    
+}
---
+++ provision/shell/inc/provision.inc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# provisionning.inc.sh
+#
+
+function start_provisionning(){
+    alert_info "Provisioning virtual machine..."
+    alert_info "$(alert_line)"
+    alert_info "You choose to install ${CMS} version 8 with the stack ${STACK}"
+    alert_info "Your project directory will be ${PROJECT_DIR} and web root ${WEB_ROOT}"
+    alert_info "It will work on php ${PHP_BASE_VERSION}"
+}
+
+function drupal_provisionning(){
+    alert_info "$(alert_line)"
+    alert_info "Provisioning Drupal..."
+    alert_info "$(alert_line)"
+
+    cd /var/www/
+    mkdir -p ${PROJECT_DIR}
+    cd "${PROJECT_DIR}"
+
+    if [[ "$(drupal_already_installed)" == "true" ]]; then
+        alert_info "Drupal 8 already installed."
+        drupal_install_dependencies
+    else
+        drupal_install
+    fi
+
+    alert_success "$(alert_line)"
+    alert_success "End Provisioning Drupal..."
+    alert_success "$(alert_line)"
+}
