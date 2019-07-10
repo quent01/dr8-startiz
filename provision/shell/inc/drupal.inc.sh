8e8eb27c0509bbd197ba39949b30e0f25c269ab6
Unknown <quentinthenoz@gmail.com>
2019-07-10 09:59:47 +0200

    feat(drupal installation) : we optimize drupal installation

--- provision/shell/inc/drupal.inc.sh
+++ provision/shell/inc/drupal.inc.sh
@@ -15,8 +15,8 @@ function drupal_already_installed(){
 function drupal_install_dependencies(){
     alert_info "Installation of Drupal 8 dependencies ..."
     alert_info "$(alert_line)"    
-    composer --global config process-timeout 2000
-    composer install --prefer-dist
+    composer --global config process-timeout 0
+    composer install --prefer-dist --optimize-autoloader
     alert_success "Drupal 8 dependencies were installed with success..."
     alert_success "$(alert_line)"
 }
@@ -24,8 +24,9 @@ function drupal_install_dependencies(){
 function drupal_install(){
     alert_info "Installation of Drupal 8..."
     alert_info "$(alert_line)"    
-    
-    composer create-project drupal-composer/drupal-project:8.x-dev ./ --stability dev --no-interaction --no-install
+    composer --global config process-timeout 0    
+    composer create-project drupal-composer/drupal-project:8.x-dev ./ --stability dev --no-interaction --no-install --prefer-dist --remove-vcs
+    composer install --prefer-dist --optimize-autoloader
     alert_success "Drupal 8 was downloaded with success."    
     drush site-install standard --db-url=mysql://${DB_USER}:${DB_PASS}@localhost/${DB_NAME} --account-name=${ADMIN_USER} --account-pass=${ADMIN_PWD} --site-name=${SITE_NAME}
 
