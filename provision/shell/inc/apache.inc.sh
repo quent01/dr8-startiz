eec44252aa516019308b58d57c8f61e6cb72cff6
Unknown <quentinthenoz@gmail.com>
2019-07-23 13:25:21 +0200

    fix(apache) : we keep the original file, and move the one in which we do the replace

--- provision/shell/inc/apache.inc.sh
+++ provision/shell/inc/apache.inc.sh
@@ -7,10 +7,16 @@
 
 function apache_configure_vhost(){
     # we modify conf file with settings
-    sed -i "s|%WEB_ROOT%|${WEB_ROOT}|g" ${PATH_PROVISION_APACHE}${FILE_APACHE_CONF}
+    sed -i.bak "s|%WEB_ROOT%|${WEB_ROOT}|g" ${PATH_PROVISION_APACHE}${FILE_APACHE_CONF}
 
     # we copy conf file to vm
     cp ${PATH_PROVISION_APACHE}${FILE_APACHE_CONF} ${PATH_A2_SITES_AVAILABLE}
+    
+    # we rest initial file
+    cd ${PATH_PROVISION_APACHE}
+    rm ${FILE_APACHE_CONF}
+    mv ${FILE_APACHE_CONF}.bak ${FILE_APACHE_CONF}
+
     sudo chmod 644 ${PATH_A2_SITES_AVAILABLE}${FILE_APACHE_CONF}
     sudo a2dissite 000-default
     sudo a2ensite ${FILE_APACHE}
